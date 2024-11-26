7623d5f78b8b934fde9b238625558deca65189f4
Marco <802.374@alunos.unigran.br>
2024-11-25 20:11:49 -0400

    Configurações do projeto

---
+++ lib/screen/homescrean.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Homescrean extends StatelessWidget {
+  const Homescrean({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
